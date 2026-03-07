fdbb9408334b83a83170e782c1c1dbdd690aaf9b
suraj12m <shikhatman123@gmail.com>
2026-03-07 23:08:48 +0530

    added create and join team module

--- backend/Evecustom/Evecustom/settings.py
+++ backend/Evecustom/Evecustom/settings.py
@@ -130,4 +130,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'studentdashboardbit@gmail.com'
 EMAIL_HOST_PASSWORD = 'xsix mibp riko xxoe'
-DEFAULT_FROM_EMAIL = 'BITcoders <no_reply@domain.com>'
+DEFAULT_FROM_EMAIL = 'Evecustom_BITcoders <no_reply@domain.com>'
